112fbd9f1ebf9314244476ffb5fb78595e96e715
Aryant Soni <aryantshrirang4@gmail.com>
2024-09-07 12:46:09 +0530

    navbar and  working process glitch fix 2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -43,27 +43,40 @@ const Navbar = () => {
                 className={` bg-black  w-full z-10 overflow-x-hidden   top-0 start-0 border-b border-none dark:border-gray-600`}
             >
                 <div className=" relative max-w-full   flex flex-wrap items-center justify-between mx-auto p-4 px-4 md:p-4 md:px-8">
-                    
+
                     <a href="/" onClick={closeMenu}>
                         <div className="flex items-center space-x-3 rtl:space-x-reverse">
-                            <img loading="lazy" src={logo} className=' w-[9rem] phone:w-[7rem] h-auto ' alt="VOIP" />
+                            <img loading="lazy" src={logo}  className=' w-[9rem] phone:w-[5.5rem] h-auto '  alt="VOIP" />
                             {/* <h1 className=' text-4xl font-bold font-outfit  text-primary-gradient phone:text-xl ' >Logo</h1> */}
                         </div>
                     </a>
 
-                    
 
-                    {/* <div className="flex  md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"> */}
+                    <div className="flex  md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                        <div className=' flex gap-3 phone:gap-1  items-center' >
+                            
+                            <Link to="/" className='rbtn h-fit rounded-full'>
+                                <span className="text rounded-full px-5 py-2 phone:py-1 phone:px-2 phone:text-sm ">Sign In</span>
+                            </Link>
+                            <Link to="/ " className='rbtn h-fit rounded-full'>
+                                <span className="text rounded-full px-5 py-2 phone:py-1 phone:px-2  phone:text-sm ">Sign Up</span>
+                            </Link>
+
+                             
+                        </div>
+
+
+
                         <button
                             onClick={toggleNav}
                             type="button"
-                            className="  inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-red-100 bg-gray-900 border border-[--primary-color] rounded-lg md:hidden   focus:outline-none focus:ring-2 focus:ring-[--prime-color]   ring-[--prime-color]  hover:bg-gray-900  "
+                            className="  inline-flex items-center p-1 w-9 h-9 justify-center text-sm text-red-100 bg-gray-900 border border-[--primary-color] rounded-lg md:hidden   focus:outline-none focus:ring-2 focus:ring-[--prime-color]   ring-[--prime-color]  hover:bg-gray-900  "
                             aria-controls="navbar-sticky1"
                             aria-expanded={isNavVisible}
                         >
                             <span className="sr-only">Open main menu</span>
                             <svg
-                                className="w-5 h-5"
+                                className="w-4 h-5"
                                 aria-hidden="true"
                                 xmlns=" "
                                 fill="none"
@@ -78,58 +91,63 @@ const Navbar = () => {
                                 />
                             </svg>
                         </button>
-                    {/* </div> */}
+                    </div>
+
+
+
 
                     <div
                         id="navbar-sticky1"
                         className={`items-center    justify-between   w-[99%] md:flex md:w-auto md:order-1 ${isNavVisible ? 'block  ' : 'hidden'
                             }`}
                     >
-                        <ul className="   navcar  flex  items-center   flex-col p-4   md:py-1.5   mt-4 font-medium border border-[--primary-color]  md:rounded-[50px] rounded-lg   md:space-x-[3vmax] rtl:space-x-reverse md:flex-row md:mt-0 md:border-0   text-white tablet:bg-gray-900    ">
+                        <ul className="   navcar  flex    flex-col p-4   md:py-1.5   mt-4 font-medium border border-[--primary-color]  md:rounded-[50px] rounded-lg   md:space-x-[3vmax] rtl:space-x-reverse md:flex-row md:mt-0 md:border-0   text-white tablet:bg-gray-900    ">
 
-                            <li className='md:border-none w-full  border-b-[1px]  border-gradient '>
+                            <li className='md:border-none border-b-[1px]  border-gradient '>
                                 <Link to="/" onClick={closeMenu}>
-                                    <div className=" font-normal block text-nowrap py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0  dark:border-gray-700 hover:text-[#8AFADE] font-outfit   ">Home</div>
+                                    <div className=" font-normal block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0  dark:border-gray-700 hover:text-[#8AFADE] font-outfit   ">Home</div>
                                 </Link>
                             </li>
 
-                            <li className='md:border-none w-full  border-b-[1px] border-gradient '>
+                            <li className='md:border-none border-b-[1px] border-gradient '>
                                 <Link to="/?=Service" onClick={closeMenu}  >
-                                    <div className=" font-normal block text-nowrap py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">Service</div>
+                                    <div className=" font-normal block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">Service</div>
                                 </Link>
                             </li>
-
-                            <li className='md:border-none w-full  border-b-[1px] border-gradient '>
+                            {/* <li className='md:border-none border-b-[1px] border-gradient '>
+                                <a to="/How-to-buy" onClick={closeMenu}>
+                                    <div className=" font-normal block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">How to buy</div>
+                                </a>
+                            </li> */}
+                            <li className='md:border-none border-b-[1px] border-gradient '>
                                 <Link to="/?=FAQ" onClick={closeMenu}>
-                                    <div className=" font-normal block text-nowrap py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">FAQ</div>
+                                    <div className=" font-normal block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">FAQ</div>
                                 </Link>
                             </li>
-                            <li className='md:border-none w-full  border-b-[1px] border-gradient '>
+                            <li className='md:border-none border-b-[1px] border-gradient '>
                                 <Link to="/?=How_it_works" onClick={closeMenu}>
-                                    <div className=" font-normal block text-nowrap py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">How it works</div>
+                                    <div className=" font-normal block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">How it works</div>
                                 </Link>
                             </li>
-                            <li className='md:border-none w-full  border-b-[1px] border-gradient '>
+                            <li className='md:border-none border-b-[1px] border-gradient '>
                                 <Link to="/?=Contact" onClick={closeMenu}>
-                                    <div className=" font-normal block text-nowrap py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">Contact</div>
+                                    <div className=" font-normal block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0       dark:border-gray-700 hover:text-[#8AFADE] font-outfit ">Contact</div>
                                 </Link>
                             </li>
 
-                            <li className='md:border-none w-full  border-b-[1px] border-gradient flex gap-3 phone:gap-1 tablet:py-3    ' >
- 
-                                <Link to="/" onClick={closeMenu} className='rbtn h-fit rounded-full'>
-                                    <span className="text rounded-full px-5 py-2 phone:py-1 phone:px-3 ">Sign In</span>
-                                </Link>
-                                 
-                                <Link to="/" onClick={closeMenu} className='rbtn h-fit rounded-full'>
-                                    <span className="text rounded-full px-5 py-2 phone:py-1 phone:px-3 ">Sign Up</span>
-                                </Link>
 
-                            </li>
+
                         </ul>
+
                     </div>
                 </div>
             </nav>
+
+
+
+
+
+
 
 
 
